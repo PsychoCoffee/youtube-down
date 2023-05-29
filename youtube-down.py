dc91c208dd3fb35d5fe49c70c65069fda82a5c8d
PsychoCoffeee <89361918+PsychoCoffee@users.noreply.github.com>
2023-05-30 01:04:01 +0200

    Update youtube-down.py
    Added legal text just in case

--- youtube-down.py
+++ youtube-down.py
@@ -1,3 +1,14 @@
+"""
+Disclaimer:
+This script is provided for educational purposes only. The use of this script to download videos from YouTube may violate YouTube's terms of service or infringe on the rights of content creators. Please use this script responsibly and ensure that you have the necessary rights or permissions to download and use the videos.
+
+By using this script, you agree that you are solely responsible for any legal consequences that may arise from your use of the script. The script author disclaims any liability or responsibility for the misuse or unauthorized use of this script.
+
+Please review and comply with YouTube's terms of service and respect the rights of content creators.
+
+Luka Beg 2023(c)
+"""
+
 from pytube import YouTube, Playlist
 
 def select_video_format(video):
